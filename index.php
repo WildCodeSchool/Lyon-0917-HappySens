f9ad43a2695531980c5fbc04cff8d402ac86513a
Alban <alban.such@gmail.com>
2017-09-27 18:17:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,6 @@
     <link rel="stylesheet" href="public/css/style.css">
     <title>HappySens</title>
 
-    <!--Let browser know website is optimized for mobile-->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
 
