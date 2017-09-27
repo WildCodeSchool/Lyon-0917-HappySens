31c2ab98b061ecbcd766a65cdcf4e33890868c0b
Banb4n <alban.such@gmail.com>
2017-09-27 23:28:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <!--  include Head  -->
-    <?php require './public/includes/head.php'?>
+    <?php require './public/includes/head.php'; ?>
 </head>
 <body>
 <main class="col s12">
@@ -13,14 +13,14 @@
     </section>
 
     <!--  include button scroll to top  -->
-    <?php require './public/includes/goToTop.php'?>
+    <?php require './public/includes/goToTop.php'; ?>
 
     <!--  include footer  -->
     <div class="row" id="footer">
-        <?php require './public/includes/footer.php'?>
+        <?php require './public/includes/footer.php'; ?>
     </div>
 </main>
     <!--  includes all required Javascript files -->
-    <?php require './public/includes/script.php'?>
+    <?php require './public/includes/script.php'; ?>
 </body>
 </html>
