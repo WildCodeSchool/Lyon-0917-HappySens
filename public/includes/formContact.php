28eff038fc46cabd06fa30b2a48e942d3efb3bd6
Aurelie-feyzin <gramusset.a@gmail.com>
2017-10-02 13:25:07 +0200

    #5 modifie contact.php add textContact.php

--- public/includes/formContact.php
+++ public/includes/formContact.php
@@ -21,21 +21,16 @@ session_start();
     </div>
     <div class="row">
         <div class="input-field col s12 m6">
-            <input placeholder="ex: martin" id="first_name" type="text" class="validate" name="first_name"
+            <i class="material-icons prefix">account_circle</i>
+            <input id="first_name" type="text" class="validate" name="first_name"
                    value="<?php echo isset($_SESSION['inputs']['first_name']) ? $_SESSION['inputs']['first_name'] : ""; ?>" required>
             <label for="first_name">Prénom</label>
         </div>
         <div class="input-field col s12 m6">
-            <input id="last_name" type="text" class="validate" placeholder="ex: Dupond" name="last_name"
+            <i class="material-icons prefix">account_circle</i>
+            <input id="last_name" type="text" class="validate" name="last_name"
                    value="<?php echo isset($_SESSION['inputs']['last_name']) ? $_SESSION['inputs']['last_name'] : ""; ?>" required>
             <label for="last_name">Nom</label>
-        </div>
-    </div>
-    <div class="row">
-        <div class="input-field col s12">
-            <input id="company_name" type="text" class="validate" placeholder="ex: Happy Sens" name="company_name"
-                   value="<?php echo isset($_SESSION['inputs']['company_name']) ? $_SESSION['inputs']['company_name'] : ""; ?>" required>
-            <label for="company_name">Entreprise</label>
         </div>
     </div>
     <div class="row">
@@ -53,8 +48,17 @@ session_start();
             <p class="invalid"><?php echo isset($_SESSION['errors']['phone']) ? $_SESSION['errors']['phone'] : "" ?></p>
         </div>
     </div>
-    <div class="row">
 
+
+    <div class="row">
+        <div class="input-field col s6">
+            <i class="material-icons prefix">domain</i>
+            <input id="company_name" type="text" class="validate"  name="company_name"
+                   value="<?php echo isset($_SESSION['inputs']['company_name']) ? $_SESSION['inputs']['company_name'] : ""; ?>" required>
+            <label for="company_name">Entreprise</label>
+        </div>
+
+        <div class="col s12 m6">
         <label for="choose">Objet du message </label>
         <select class="browser-default" id="choose" name="choose" required>
             <option value="" disabled selected>Votre choix</option>
@@ -65,15 +69,15 @@ session_start();
             <option value="3"
                 <?php echo ((($_SESSION['inputs']['choose']) AND ($_SESSION['inputs']['choose']) === '3') ? "selected" : ""); ?>>Option 3</option>
         </select>
+        </div>
 
-        <br>
 
     </div>
 
     <div class="row">
         <div class="input-field col s12">
             <i class="material-icons prefix">mode_edit</i>
-            <textarea id="content" class="materialize-textarea" name="content"><?php echo isset($_SESSION['errors']['content']) ? "" : $_SESSION['inputs']['content']?></textarea>
+            <textarea id="content" class="materialize-textarea" name="content" ><?php echo isset($_SESSION['errors']['content']) ? "" : $_SESSION['inputs']['content']?></textarea>
             <label for="content">Votre message :</label>
         </div>
     </div>
--- public/pages/contact.php
+++ public/pages/contact.php
@@ -7,32 +7,17 @@
 </head>
 <body>
 <main class="col s12">
-    <h1>JE SUIS MATERIALIZE</h1>
+
 
     <?php include "../includes/jumbotron.php"; ?>
-    <section class="Work">
 
 
-     <div class="row">
-        <div class="col s6 center-align">
-            <p>Harum trium sententiarum nulli prorsus assentior. Nec enim illa prima vera est, ut, quem ad modum in se quisque sit, sic in amicum sit animatus. Quam multa enim, quae nostra causa numquam faceremus, facimus causa amicorum! precari ab indigno, supplicare, tum acerbius in aliquem invehi insectarique vehementius, quae in nostris rebus non satis </p>
-               <p>Nec minus feminae quoque calamitatum participes fuere similium. nam ex hoc quoque sexu peremptae sunt originis altae conplures, adulteriorum flagitiis obnoxiae vel stuprorum. inter quas notiores fuere Claritas et Flaviana, quarum altera cum duceretur ad mortem, indumento, quo vestita erat, abrepto, ne velemen quidem secreto membrorum sufficiens </p>
-               <p>Et prima post Osdroenam quam, ut dictum est, ab hac descriptione discrevimus, Commagena, nunc Euphratensis, clementer adsurgit, Hierapoli, vetere Nino et Samosata civitatibus amplis inlustris.
-            </p>
-
+    <section class="Contact">
+        <div class="row">
+            <?php include "../includes/textContact.php"; ?>
+            <?php include "../includes/formContact.php"; ?>
         </div>
-
-
-         <?php include "../includes/formContact.php"; ?>
-         </div>
-
-
-
-
-
     </section>
-
-
 
 
     <!--  include button scroll to top  -->
