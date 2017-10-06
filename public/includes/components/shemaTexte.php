8ced0e2fe065d0c6f5b8043e292772da2aba74f8
Banb4n <alban.such@gmail.com>
2017-10-06 09:18:56 +0200

    ADD new pages

--- public/includes/components/shemaTexte.php
+++ public/includes/components/shemaTexte.php
@@ -2,16 +2,16 @@
     <div class="col s12 l6">
         <div class="card center">
             <div class="card-content blue-text">
-                <span class="cardTitle">Shema du processus</span>
-                            <p class="para1 black-text">Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.</p>
+                <span class="cardTitle">Schema du processus</span>
+                <p class="para1 black-text">Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.</p>
             </div>
         </div>
     </div>
     <div class="col s12 l6">
-        <div class="card center">
-            <div class="card-content blue-text">
+        <div class="card center blue darken-2">
+            <div class="card-content white-text">
                 <span class="cardTitle">Les grandes étapes</span>
-                           <p class="para1 black-text">Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.</p>
+                <p class="para1 white-text">Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.</p>
            </div>
         </div>
     </div>
