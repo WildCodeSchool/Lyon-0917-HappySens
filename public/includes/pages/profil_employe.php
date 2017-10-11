b33c944fe83a8edf7f9d714b0d1f34efb0bf1384
Manuel Briand <manuel.briand354@gmail.com>
2017-10-11 11:06:10 +0200

    add navbar page_employe

--- public/includes/pages/profil_employe.php
+++ public/includes/pages/profil_employe.php
@@ -1,28 +1,120 @@
-<?php
-session_start();
-?>
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
 <head>
-
-    <!--  include Head  -->
-    <?php require 'public/includes/structure/head.php'; ?>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
 </head>
 <body>
-<nav>
-    <div class="nav-wrapper">
-        <a href="#" class="brand-logo">Logo</a>
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a href="#" class="waves-effect waves-light btn darken-2">Deconnexion<i class="material-icons right">person</i></a></li>
+<header>
 
-        </ul>
+    <nav>
+        <div class="nav-wrapper">
+            <a href="#" class="brand-logo center">Logo</a>
+            <ul id="slide-out" class="side-nav">
+                <li><div class="user-view">
+                        <div class="background">
+                            <img src="images/office.jpg">
+                        </div>
+                        <a href="#!user"><img class="circle" src="images/yuna.jpg"></a>
+                        <a href="#!name"><span class="white-text name">John Doe</span></a>
+                        <a href="#!email"><span class="white-text email">jdandturk@gmail.com</span></a>
+                    </div></li>
+                <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
+                <li><a href="#!">Second Link</a></li>
+                <li><div class="divider"></div></li>
+                <li><a class="subheader">Subheader</a></li>
+                <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+            </ul>
+            <a href="#" data-activates="slide-out" class="button"><i class="material-icons">menu</i></a>
+        </div>
+    </nav>
+
+</header>
+
+<div class="container">
+    <div class="row">
+        <div class="col s12 center">
+            <h4>Le bonheur au travail commence par un projet qui fait sens !</h4>
+        </div>
     </div>
-</nav>
+    <div class="row">
+        <div class="col s2">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7jLbL0dui40KxfsmS915P42beTNKVX2fO64gGCsV_sc4EhcarYw"
+                 style="width: 150px"
+                 class="circle responsive-img">
+        </div>
+        <div class="col s2 offset-s8">
+            <img src="http://proxymedia.woopic.com/api/v1/images/813%2F/actu-drupal/20f/355/dd3d5f5c8acddc399f10ecf059/Logo1_Orange.jpg"
+                 style="width: 100px">
+        </div>
+    </div>
+    <div class="col s12">
+        <h4>Michel Dupont</h4>
+    </div>
+        <div class="row">
 
+            <div class="col s6">
 
-
-
-<!--  includes all required Javascript files -->
-<?php require 'public/includes/structure/script.php'; ?>
+                <div class="col s12">
+                    <p>Fonction : happy-officer</p>
+                </div>
+                <div class="col s12">
+                    <p>Age : 32 ans</p>
+                </div>
+                <div class="col s12">
+                    <p>Lieu de travail : Lyon</p>
+                </div>
+                <div class="col s12">
+                    <p>Téléphone : 06 55 76 92 58</p>
+                </div>
+                <div class="col s12">
+                    <p>Mail : michel.dupont@gmail.com</p>
+                </div>
+                <div class="col s12">
+                    <p>Me suivre : <img
+                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdHaBbzxl5ZsM_68xadstvMpS7FJbzmqVPayx4DAybTv-kclpczQ"
+                                style="width: 100px" alt=""></p>
+                </div>
+            </div>
+            <div class="col s6">
+                <div class="col s12">
+                    <p>
+                        Slogan (à completer): <br>
+                        Au travail je me sens happy quand
+                        <span> il y a des frites à la cantine.</span>
+                    </p>
+                </div>
+                <div class="col s12">
+                    <p>
+                        Présentation/Bio (paragraphe) : <br>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br>
+                        Quisque ornare iaculis leo eu faucibus. <br>
+                        Vestibulum ac tellus ultricies ex suscipit rhoncus.
+                    </p>
+                </div>
+                <div class="col s12">
+                    <p>
+                        Langues parlées par ordre de préférence : <br>
+                    </p>
+                    <ul>
+                        <li>Anglais</li>
+                        <li>Espagnol</li>
+                        <li>Latin</li>
+                    </ul>
+                    </p
+                </div>
+            </div>
+        </div>
+</div>
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+<script src="../../script/sideNav.js"></script>
 </body>
 </html>
+
