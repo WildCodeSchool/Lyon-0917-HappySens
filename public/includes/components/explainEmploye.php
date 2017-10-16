37286bdae97df11a5b19ae8f1b215726b6da5cb4
Banb4n <alban.such@gmail.com>
2017-10-16 17:09:03 +0200

    Add new components

---
+++ public/includes/components/explainEmploye.php
@@ -0,0 +1,21 @@
+<div class="container">
+    <div class="col s12 m12 l12 blocExplainEntreprise">
+        <h3 class="center amber-text text-darken-2"> HappySens vous permet d’être acteur et force de proposition en matière de bonheur au travail</h3>
+
+        <div class="containExplainEntreprise">
+            <p class="detailsExplainEntreprise blue-text text-darken-3">Qui mieux que vous peut savoir ce qui vous rend plus heureux ? Quelle passion, talent ou projet personnel avez-vous laissé de côté pour vous consacrer à votre carrière professionnelle ? Faut-il attendre d’être à la retraite pour s’épanouir et réaliser ses rêves ? </p>
+        </div>
+
+        <div class="containExplainEntreprise">
+            <p class="detailsExplainEntreprise blue-text text-darken-3">Pour en finir avec des solutions descendantes, éparses et peut ciblées, HappySens vous place au pilotage des projets bien-être et bonheur au travail.</p>
+        </div>
+
+        <div class="containExplainEntreprise">
+            <p class="detailsExplainEntreprise blue-text text-darken-3">Avec notre plateforme collaborative et notre programme d’accompagnement, le bonheur peut commencer dès aujourd’hui, dans votre entreprise et avec vos collègues.</p>
+        </div>
+
+        <div class="containExplainEntreprise">
+            <h5 class="titleExplainEntreprise amber-text text-darken-3">Vous avez bien plus qu’un métier à partager !</h5>
+        </div>
+    </div>
+</div>
