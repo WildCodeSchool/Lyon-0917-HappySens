084c83d4bf47ce0d885f06a24a68d8a89f81488e
Fantasia Guilloton <fantasia.g1994@gmail.com>
2017-10-17 09:35:35 +0200

    fix script

--- add_project.php
+++ add_project.php
@@ -158,6 +158,8 @@
 
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+
+
 <script type="text/javascript" src="public/script/datepicker.js"></script>
 <script type="text/javascript" src="public/script/app.js"></script>
 </body>
