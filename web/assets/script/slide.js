740e443c6a3a1f32bb976fab60673f779cb6ff12
Fantasia Guilloton <fantasia.g1994@gmail.com>
2017-10-26 17:42:33 +0200

    slider index

---
+++ web/assets/script/slide.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slider').slider({
+        interval: 2700,
+        transition: 700
+    });
+    // Next slide
+    $('#next').slider('next');
+// Previous slide
+    $('#prev').slider('prev');
+
+    $()
+});
