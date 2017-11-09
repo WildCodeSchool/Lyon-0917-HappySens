c4821195fda485729c54ce856b8ae1c082fbdb4b
Banb4n <alban.such@gmail.com>
2017-11-09 17:46:06 +0100

    Update queries

--- web/assets/script/scrollTop.js
+++ web/assets/script/scrollTop.js
@@ -1,20 +1,15 @@
 $(document).ready(function() {
-    var duration = 350;
+    var duration = 800;
     $(window).scroll(function() {
         if ($(this).scrollTop() > 50) {
-            // Si un défillement de 50 pixels ou plus.
-            // Ajoute le bouton
             $('.scrollToTop').fadeIn(duration);
         } else {
-            // Sinon enlève le bouton
             $('.scrollToTop').fadeOut(duration);
         }
     });
-
     $('.scrollToTop').click(function(event) {
-        // Un clic provoque le retour en haut animé.
         event.preventDefault();
         $('html, body').animate({scrollTop: 0}, duration);
         return false;
-    })
+    });
 });
