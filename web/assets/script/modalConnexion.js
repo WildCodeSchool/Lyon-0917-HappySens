9b15b2c558bb9adc40ad852a06a18a510e84b940
Aurelie-feyzin <gramusset.a@gmail.com>
2017-11-10 15:16:12 +0100

    add salary first connexion

--- web/assets/script/modalConnexion.js
+++ web/assets/script/modalConnexion.js
@@ -8,4 +8,12 @@ function findPassword(){
     $('#hiddenMissingPassword').addClass('hidden');
     $('#password').removeClass('hidden');
     $('#btnConnexion').removeClass('hidden');
+    $('#hiddenNewSalary').addClass('hidden');
+}
+
+
+function newSalary(){
+    $('#hiddenNewSalary').removeClass('hidden');
+    $('#password').addClass('hidden');
+    $('#btnConnexion').addClass('hidden');
 }
