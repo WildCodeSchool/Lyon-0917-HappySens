90789428b66b5ab53b1e6c5e55e23193983604b7
Aurelie-feyzin <gramusset.a@gmail.com>
2017-12-05 15:48:58 +0100

    -Add Regex for phone -Transfer code in Controller to Service -Add email in viewTwig

--- src/AppBundle/Controller/EmailContactController.php
+++ src/AppBundle/Controller/EmailContactController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\EmailContact;
 use AppBundle\Form\EmailContactType;
+use AppBundle\Service\EmailService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,30 +17,13 @@ class EmailContactController extends Controller
      * @param \Swift_Mailer $mailer
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function contactAction(Request $request, \Swift_Mailer $mailer)
+    public function contactAction(Request $request, EmailService $emailService)
     {
         $contact = new EmailContact();
         $form = $this->createForm(EmailContactType::class, $contact);
         $form->handleRequest($request);
         if ($form->isSubmitted() and $form->isValid()) {
-            $message = (new \Swift_Message('Contact HappySens'))
-                ->setFrom($contact->getEmail(), $contact->getFirstName() .  $contact->getLastName())
-                ->setTo('famar.wcslyon@gmail.com')
-                ->setBcc($contact->getEmail(), 'contact HappySens')
-                ->setReplyTo('famar.wcslyon@gmail.com')
-                ->setBody(
-                    $this->renderView('notificationsEmail/contact.html.twig', [
-                        'firstName' => $contact->getFirstName(),
-                        'lastName' => $contact->getLastName(),
-                        'phone' => $contact->getPhone(),
-                        'company' => $contact->getNameCompany(),
-                        'status' => $contact->getStatus(),
-                        'message' => $contact->getMessage()
-                    ]),
-                    'text/html'
-                );
-            $mailer->send($message);
-
+            $emailService->sendMessageContact($contact);
             $this->addFlash(
                 'contact',
                 'Merci, votre message a bien été envoyé.'
--- src/AppBundle/Service/EmailService.php
+++ src/AppBundle/Service/EmailService.php
@@ -11,6 +11,36 @@ namespace AppBundle\Service;
 
 class EmailService
 {
+    protected $mailer;
+    protected $template;
 
+    public function __construct(\Swift_Mailer $mailer, \Twig_Environment $template)
+    {
+        $this->mailer = $mailer;
+        $this->template = $template;
+    }
+
+    public function sendMessageContact($contact)
+    {
+        $message = \Swift_Message::newInstance()
+            ->setSubject('Merci d\'avoir contacté HappySens')
+            ->setFrom($contact->getEmail(), $contact->getFirstName() . ' ' . $contact->getLastName())
+            ->setTo('famar.wcslyon@gmail.com')
+            ->setBcc($contact->getEmail(), 'contact HappySens')
+            ->setReplyTo('famar.wcslyon@gmail.com')
+            ->setBody(
+                $this->template->render('notificationsEmail/contact.html.twig', [
+                    'firstName' => $contact->getFirstName(),
+                    'lastName' => $contact->getLastName(),
+                    'email'=> $contact->getEmail(),
+                    'phone' => $contact->getPhone(),
+                    'company' => $contact->getNameCompany(),
+                    'status' => $contact->getStatus(),
+                    'message' => $contact->getMessage()
+                ]),
+                'text/html'
+            );
+        $this->mailer->send($message);
+    }
 
 }
