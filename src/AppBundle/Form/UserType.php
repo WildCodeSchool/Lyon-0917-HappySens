547da7791125c12726df6d979043c748b732e070
Aurelie-feyzin <gramusset.a@gmail.com>
2017-12-18 11:12:49 +0100

    fix bug langages

--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -2,9 +2,12 @@
 
 namespace AppBundle\Form;
 
+use AppBundle\Entity\Language;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\LanguageType;
 
 class UserType extends AbstractType
 {
@@ -13,7 +16,29 @@ class UserType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('firstName')->add('lastName')->add('phone')->add('email')->add('status')->add('birthdate')->add('photo')->add('biography')->add('slogan')->add('password')->add('mood')->add('job')->add('workplace')->add('nativeLanguage')->add('language')->add('company')->add('isActive');
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('phone')
+            ->add('email')
+            ->add('status')
+            ->add('birthdate')
+            ->add('photo')
+            ->add('biography')
+            ->add('slogan')
+            ->add('password')
+            ->add('mood')
+            ->add('job')
+            ->add('workplace')
+            ->add('nativeLanguage')
+            ->add('languagesUser', EntityType::class, [
+            'class' => Language::class,
+            'required' => false,
+            'empty_data' => null,
+            'multiple' => true,
+        ])
+            ->add('company')
+            ->add('isActive');
     }
     
     /**
