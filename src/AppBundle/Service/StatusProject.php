66f4e0f63b821bcfe7bea028faa56e5d9171ac40
Aurelie-feyzin <gramusset.a@gmail.com>
2017-12-21 18:12:11 +0100

    Revert "Profil happy coach1"

--- src/AppBundle/Service/StatusProject.php
+++ src/AppBundle/Service/StatusProject.php
@@ -15,8 +15,8 @@ class StatusProject
     public function getStatusTwig($status)
     {
         $statusTwig = [
-            'class' => "",
-            'text' => ""
+        'class' => "",
+        'text' => ""
         ];
         switch (true) {
             case $status == 1:
@@ -33,60 +33,6 @@ class StatusProject
                 break;
         }
         return $statusTwig;
-    }
-
-    public function getProjectsWithStatus($projects)
-    {
-        for ($i = 0; $i < count($projects); $i++) {
-            $status = $projects[$i]->getStatus();
-            $statusTwig = [
-                'class' => "",
-                'text' => ""
-            ];
-            switch (true) {
-                case $status == 1:
-                    $statusTwig['class'] = "chip inProgress";
-                    $statusTwig['text'] = 'En attente';
-                    break;
-                case $status == 2:
-                    $statusTwig['class'] = "chip validate";
-                    $statusTwig['text'] = 'En cours';
-                    break;
-                case $status == 3:
-                    $statusTwig['class'] = "chip end";
-                    $statusTwig['text'] = 'Terminé';
-                    break;
-            }
-            $projects[$i]->setStatus(['class' => $statusTwig['class'], 'text' => $statusTwig['text']]);
-        }
-
-        return $projects;
-    }
-
-    public function getProjectWithStatus($project)
-    {
-            $status = $project->getStatus();
-            $statusTwig = [
-                'class' => "",
-                'text' => ""
-            ];
-            switch (true) {
-                case $status == 1:
-                    $statusTwig['class'] = "chip inProgress";
-                    $statusTwig['text'] = 'En attente';
-                    break;
-                case $status == 2:
-                    $statusTwig['class'] = "chip validate";
-                    $statusTwig['text'] = 'En cours';
-                    break;
-                case $status == 3:
-                    $statusTwig['class'] = "chip end";
-                    $statusTwig['text'] = 'Terminé';
-                    break;
-            }
-            $project->setStatus(['class' => $statusTwig['class'], 'text' => $statusTwig['text']]);
-
-        return $project;
     }
 
 }
