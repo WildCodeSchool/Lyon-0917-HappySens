eef2b2d5ea7486f588d2bfecd251a939a32eea68
Banb4n <alban.such@gmail.com>
2018-01-09 18:36:09 +0100

    Update type of input in view change

--- src/AppBundle/Form/EditPasswordType.php
+++ src/AppBundle/Form/EditPasswordType.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,7 @@ class EditPasswordType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('pwd')->add('confirmPwd');
+        $builder->add('pwd', PasswordType::class)->add('confirmPwd', PasswordType::class);
     }
     
     /**
