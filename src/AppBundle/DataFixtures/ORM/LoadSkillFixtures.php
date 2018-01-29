92e2be5440ae6aeb8bf4b3d9cfa0de2b7ac079b7
Fantasia Guilloton <fantasia.g1994@gmail.com>
2018-01-29 10:31:43 +0100

    add skill in fixture

--- src/AppBundle/DataFixtures/ORM/LoadSkillFixtures.php
+++ src/AppBundle/DataFixtures/ORM/LoadSkillFixtures.php
@@ -16,16 +16,16 @@ use Doctrine\Common\Persistence\ObjectManager;
 
 class LoadSkillFixtures extends Fixture implements FixtureInterface
 {
-    const MAX = 37;
+    const MAX = 41;
 
     public function load(ObjectManager $manager) {
         $skills = [];
-        $listSkill = ['Bien-être','Bricolage', 'Broderie', 'Cause animale', 'Cause humanitaire', 'Chant',
-'Cinéma', 'Cirque', 'Collection d\'objet', 'Couture','Cuisine', 'Culture et jardinage', 'Danse', 'Décoration',
- 'Dessin', 'Ecologie','Ecriture', 'Gastronomie', 'Gravure', 'Jeux vidéos','Littérature','Maquettisme', 'Musique',
-  'Oenologie', 'Peinture', 'Photographie','Sculpture','Spectacle', 'Sport animalier', 'Sport aérien', 'Sport collectif',
-  'Sport de combat', 'Sport handisport', 'Sport individuel', 'Sport mécanique', 'Sport de montagne','Sport nautique',
-   'Théâtre','Tricot' ];
+        $listSkill = ['Autres', 'Bien-être','Bricolage', 'Broderie', 'Cause animale', 'Cause humanitaire', 'Chant',
+'Cinéma', 'Cirque', 'Collection d\'objet', 'Couture', 'Cuisine', 'Culture', 'Culture et jardinage', 'Danse', 'Décoration',
+ 'Dessin', 'Ecologie','Ecriture', 'Gastronomie', 'Gravure', 'Informatique', 'Jeux vidéos', 'Littérature','Maquettisme', 'Musique',
+  'Oenologie', 'Peinture', 'Photographie', 'Sculpture', 'Spectacle', 'Sport animalier', 'Sport aérien', 'Sport collectif',
+  'Sport de combat', 'Sport handisport', 'Sport individuel', 'Sport mécanique', 'Sport de montagne', 'Sport nautique',
+   'Théâtre','Tricot', 'Voyage'];
 
         for($i = 0; $i <= self::MAX; $i++) {
             $skills[$i] = new Skill();
